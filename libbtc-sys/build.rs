472c197f3def1396ee2c5bf98f354d1d2abdf695
AtsukiTak <takatomgoo@gmail.com>
2018-03-27 23:21:37 +0900

    fix : pkg-config emit cargo meta info while compile libbtc-sys

--- libbtc-sys/build.rs
+++ libbtc-sys/build.rs
@@ -7,7 +7,6 @@ fn main() {
     let lib = pkg_config::Config::new()
         .atleast_version("2.0.0")
         .statik(true)
-        .cargo_metadata(false)
         .probe("libevent")
         .unwrap();
     for inc_path in lib.include_paths.iter() {
